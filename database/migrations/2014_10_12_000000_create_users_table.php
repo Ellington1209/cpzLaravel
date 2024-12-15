21bfb4658a4645e161b9a664b7de8b17f069d571
Ellington Machado <ellington.machado@verum.global>
2024-12-15 09:54:34 -0300

    gitignore

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,6 +14,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('nome_fantasia');
+            $table->string('telefone',14);
+            $table->string('whatsapp',14);
+            $table->string('name_user')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
